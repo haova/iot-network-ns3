26b17878f3b3d95ac10bc02679be1f18ea332c51
nhatndmuit <nhatndm.17@grad.uit.edu.vn>
2023-11-22 19:53:06 +0700

    Create server app, add external libraries

---
+++ server-app.h
@@ -0,0 +1,53 @@
+#include "ns3/application.h"
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/stats-module.h"
+
+using namespace ns3;
+
+/**
+ * ServerApp application.
+ */
+class ServerApp : public Application
+{
+  public:
+    /**
+     * \brief Get the type ID.
+     * \return The object TypeId.
+     */
+    static TypeId GetTypeId();
+
+    ServerApp();
+    ~ServerApp() override;
+
+    /**
+     * Set the counter calculator for received packets.
+     * \param calc The CounterCalculator.
+     */
+    void SetCounter(Ptr<CounterCalculator<>> calc);
+
+    /**
+     * Set the delay tracker for received packets.
+     * \param delay The Delay calculator.
+     */
+    void SetDelayTracker(Ptr<TimeMinMaxAvgTotalCalculator> delay);
+
+  protected:
+    void DoDispose() override;
+
+  private:
+    void StartApplication() override;
+    void StopApplication() override;
+
+    /**
+     * Receive a packet.
+     * \param socket The receiving socket.
+     */
+    void Receive(Ptr<Socket> socket);
+
+    Ptr<Socket> m_socket; //!< Receiving socket
+    uint32_t m_port{0};   //!< Listening port
+
+    Ptr<CounterCalculator<>> m_calc;           //!< Counter of the number of received packets
+    Ptr<TimeMinMaxAvgTotalCalculator> m_delay; //!< Delay calculator
+};
